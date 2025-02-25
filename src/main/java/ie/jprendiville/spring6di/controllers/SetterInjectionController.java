5be92d7e8af12d67abbfa9c87c819b29ee5f0a83
John <jpkeyboarder@gmail.com>
2025-02-25 19:08:54 +0000

    36: Converting the project to use DI using Spring Framework.

--- src/main/java/ie/jprendiville/spring6di/controllers/SetterInjectionController.java
+++ src/main/java/ie/jprendiville/spring6di/controllers/SetterInjectionController.java
@@ -1,15 +1,22 @@
 package ie.jprendiville.spring6di.controllers;
 
 import ie.jprendiville.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectionController {
+
+
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
     public String sayGreeting() {
+        System.out.println("This is now a Spring managed Component, using the @Component and @Autowired stereotypes.\n Spring will know that we need to autowire that automatically, but again, this is not recommended for Setter Injection.");
         return greetingService.sayGreeting();
     }
 
--- src/test/java/ie/jprendiville/spring6di/controllers/SetterInjectionControllerTest.java
+++ src/test/java/ie/jprendiville/spring6di/controllers/SetterInjectionControllerTest.java
@@ -3,22 +3,26 @@ package ie.jprendiville.spring6di.controllers;
 import ie.jprendiville.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SetterInjectionControllerTest {
 
+    @Autowired
     SetterInjectionController setterInjectionController = new SetterInjectionController();
 
-    @BeforeEach
-    void setUp() {
-        setterInjectionController = new SetterInjectionController();
-        setterInjectionController.setGreetingService(new GreetingServiceImpl());
-    }
+//    @BeforeEach
+//    void setUp() {
+//        setterInjectionController = new SetterInjectionController();
+//        setterInjectionController.setGreetingService(new GreetingServiceImpl());
+//    }
 
     @Test
     void sayGreeting() {
-        System.out.println("Setter Injection without Spring. The least preferred way along with Property Injection.");
+        System.out.println("This SetterInjected Test is now a Spring managed Component, using the @SpringBootTest and @Autowired stereotypes.\n JUnit will know that we need to autowire that automatically.\n Also we no longer need the setUp()");
         System.out.println(setterInjectionController.sayGreeting());
     }
 }
